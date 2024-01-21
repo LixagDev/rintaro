225ed0f4c3c2d3aeb39c566f1d705a2471e90473
Lixag <valent1.arn@gmail.com>
2024-01-21 14:24:54 +0100

    Create data.js

---
+++ data/data.js
@@ -0,0 +1,10 @@
+import {Rotate3D, TestTube} from "lucide-react";
+
+export function SoftwaresData(){
+    const softwares = [
+        {id: 0, name: "Convertisseur", description: "Convertissez vos images en l'extension que vous souhaitez.", href: "/convert", logo: <Rotate3D size={40}/>},
+        {id: 1, name: "Test", description: "Description", href: "/tes", logo: <TestTube size={40}/>},
+    ];
+
+    return softwares;
+}
