4ddab598b6b77b3ce6f30e0065ba5735c0e4e0ca
Lixag <valent1.arn@gmail.com>
2024-01-21 19:55:23 +0100

    Add App Toto

--- data/data.js
+++ data/data.js
@@ -4,6 +4,7 @@ export function SoftwaresData(){
     const softwares = [
         {id: 0, name: "Convertisseur", description: "Convertissez vos images en l'extension que vous souhaitez.", href: "/convert", logo: <Rotate3D size={40}/>},
         {id: 1, name: "Test", description: "Description", href: "/tes", logo: <TestTube size={40}/>},
+        {id: 1, name: "Toto", description: "Description", href: "/tes", logo: <TestTube size={40}/>},
     ];
 
     return softwares;
