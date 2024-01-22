9ad069bb23e00c8f3c1d5d1ebd96257412dea1a9
Lixag <valent1.arn@gmail.com>
2024-01-22 12:25:25 +0100

    Update data.js

--- data/data.js
+++ data/data.js
@@ -1,8 +1,8 @@
-import {Rotate3D, TestTube} from "lucide-react";
+import {RefreshCw} from "lucide-react";
 
 export function SoftwaresData(){
     const softwares = [
-        {id: 0, name: "Convertisseur", description: "Convertissez vos images en l'extension que vous souhaitez.", href: "/convert", logo: <Rotate3D size={40}/>},
+        {id: 0, name: "Convertisseur", description: "Convertissez vos images en l'extension que vous souhaitez.", href: "/convert", logo: <RefreshCw size={40}/>, beta: true},
     ];
 
     return softwares;
