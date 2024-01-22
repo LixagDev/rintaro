2902f01a5be60e9ee93a340796091061756aa29f
Lixag <valent1.arn@gmail.com>
2024-01-22 12:26:06 +0100

    Update Convert.jsx

--- components/pages/Convert.jsx
+++ components/pages/Convert.jsx
@@ -2,14 +2,19 @@
 import Navbar from "@/components/navigation/Navbar";
 import {ThemeToggle} from "@/components/theme/ThemeToggle";
 import Converter from "@/components/softwares/Converter";
+import {SoftwaresData} from "@/data/data";
+import SoftwareTitle from "@/components/softwares/SoftwareTitle";
 
 export default function Convert({...props}){
     const session = props.session;
+    const actualSoftwareName = SoftwaresData()[0].name;
+    const actualSoftwareDescription = SoftwaresData()[0].description;
 
     return(
         <>
             <div className={"flex flex-col gap-7"}>
                 <Navbar session={session}/>
+                <SoftwareTitle title={actualSoftwareName} description={actualSoftwareDescription}/>
                 <Converter/>
             </div>
             <ThemeToggle/>
