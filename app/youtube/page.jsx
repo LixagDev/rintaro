a716fda630faed4cca0983f6fecec1c2fb207355
Lixag <valent1.arn@gmail.com>
2024-01-23 22:14:40 +0100

    Update page.jsx

--- app/youtube/page.jsx
+++ app/youtube/page.jsx
@@ -1,5 +1,11 @@
+import Youtube from "@/components/pages/Youtube";
+import {getServerSession} from "next-auth";
+import {authOptions} from "@/pages/api/auth/[...nextauth]";
+import {redirect} from "next/navigation";
+
 export default async function index(){
-    return(
-        <div>youtube</div>
-    );
+    const session = await getServerSession(authOptions);
+
+    if (session) return <Youtube session={session} />;
+    else redirect("/");
 }
