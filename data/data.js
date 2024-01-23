7a64bef8f8bb268e1f433dec98a4fa26eccd57bd
Lixag <valent1.arn@gmail.com>
2024-01-23 22:14:17 +0100

    Update data.js

--- data/data.js
+++ data/data.js
@@ -3,7 +3,7 @@ import {RefreshCw, Youtube} from "lucide-react";
 export function SoftwaresData(){
     const softwares = [
         {id: 0, name: "Convertisseur", description: "Convertissez vos images en l'extension que vous souhaitez.", href: "/convert", logo: <RefreshCw size={40}/>, beta: true},
-        {id: 0, name: "Youtube WEBM", description: "Convertissez vos vidéos Youtube en WEBM.", href: "/youtube", logo: <Youtube size={40}/>, beta: true},
+        {id: 0, name: "Youtube DL", description: "Téléchargez vos videos Youtube au format MP4 ou WEBM.", href: "/youtube", logo: <Youtube size={40}/>, beta: false},
     ];
 
     return softwares;
