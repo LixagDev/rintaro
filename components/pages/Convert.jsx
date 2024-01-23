1e1030e236a0c76b7dc72b4e3a4aab1d5a3b5dba
Lixag <valent1.arn@gmail.com>
2024-01-23 22:14:38 +0100

    Update Convert.jsx

--- components/pages/Convert.jsx
+++ components/pages/Convert.jsx
@@ -4,6 +4,7 @@ import {ThemeToggle} from "@/components/theme/ThemeToggle";
 import Converter from "@/components/softwares/Converter";
 import {SoftwaresData} from "@/data/data";
 import SoftwareTitle from "@/components/softwares/SoftwareTitle";
+import SearchMenu from "@/components/SearchMenu";
 
 export default function Convert({...props}){
     const session = props.session;
@@ -18,6 +19,7 @@ export default function Convert({...props}){
                 <Converter/>
             </div>
             <ThemeToggle/>
+            <SearchMenu/>
         </>
     )
 }
