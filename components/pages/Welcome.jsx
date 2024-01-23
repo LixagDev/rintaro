68c3740e0eb8ede285f2a6520d3a880583c98b00
Lixag <valent1.arn@gmail.com>
2024-01-23 22:14:34 +0100

    Update Welcome.jsx

--- components/pages/Welcome.jsx
+++ components/pages/Welcome.jsx
@@ -1,19 +1,23 @@
 "use client"
 import AuthButton from "@/components/AuthButton";
 import {DisableContextMenu} from "@/functions/UI";
+import {ThemeToggle} from "@/components/theme/ThemeToggle";
 
 export default function Welcome({...props}){
     DisableContextMenu();
 
     return(
-        <div className={"flex h-full"}>
-            <div className={"flex flex-col basis-1/2 justify-center items-center"}>
-                <h1 className={"font-black text-7xl"}>Rintaro</h1>
-                <h2 className={"font-medium text-xl"}>Une application simple, efficace, toute en un.</h2>
+        <>
+            <div className={"flex h-full"}>
+                <div className={"flex flex-col basis-1/2 justify-center items-center"}>
+                    <h1 className={"font-black text-7xl"}>Rintaro</h1>
+                    <h2 className={"font-medium text-xl"}>Une application simple, efficace, toute en un.</h2>
+                </div>
+                <div className={"flex basis-1/2 justify-center items-center"}>
+                    <AuthButton session={props.session} providers={props.providers}></AuthButton>
+                </div>
             </div>
-            <div className={"flex basis-1/2 justify-center items-center"}>
-                <AuthButton session={props.session} providers={props.providers}></AuthButton>
-            </div>
-        </div>
+            <ThemeToggle/>
+        </>
     );
 }
