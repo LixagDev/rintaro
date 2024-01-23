9ee250b8c2d9a5fae207996876d5a4dd066a7c1d
Lixag <valent1.arn@gmail.com>
2024-01-23 13:24:02 +0100

    Latest

---
+++ app/youtube/page.jsx
@@ -0,0 +1,5 @@
+export default async function index(){
+    return(
+        <div>youtube</div>
+    );
+}
--- data/data.js
+++ data/data.js
@@ -1,8 +1,9 @@
-import {RefreshCw} from "lucide-react";
+import {RefreshCw, Youtube} from "lucide-react";
 
 export function SoftwaresData(){
     const softwares = [
         {id: 0, name: "Convertisseur", description: "Convertissez vos images en l'extension que vous souhaitez.", href: "/convert", logo: <RefreshCw size={40}/>, beta: true},
+        {id: 0, name: "Youtube WEBM", description: "Convertissez vos vidéos Youtube en WEBM.", href: "/youtube", logo: <Youtube size={40}/>, beta: true},
     ];
 
     return softwares;
