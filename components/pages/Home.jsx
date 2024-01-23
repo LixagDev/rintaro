67ee1a7005e6bfea65b118b1b6de33bfaf969698
Lixag <valent1.arn@gmail.com>
2024-01-23 22:14:35 +0100

    Update Home.jsx

--- components/pages/Home.jsx
+++ components/pages/Home.jsx
@@ -18,7 +18,7 @@ export default function Home({...props}){
                 <Softwares/>
             </div>
             <ThemeToggle/>
-            <SearchMenu></SearchMenu>
+            <SearchMenu/>
         </>
     );
 }
