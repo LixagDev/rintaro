99963de1acf4296171c22a46041f9b789bbaa611
Lixag <valent1.arn@gmail.com>
2024-01-23 22:14:30 +0100

    Update SoftwareTitle.jsx

--- components/softwares/SoftwareTitle.jsx
+++ components/softwares/SoftwareTitle.jsx
@@ -5,9 +5,9 @@ export default function SoftwareTitle({...props}){
     const softwareDescription = props.description;
 
     return(
-        <div className={"flex flex-col items-center justify-center w-full"}>
+        <div className={"flex flex-col items-center justify-center"}>
             <h2 className={"font-bold text-5xl"}>{softwareTitle}</h2>
-            <h2 className={"font-medium textt-3xl"}>{softwareDescription}</h2>
+            <h2 className={"font-medium text-xl text-center"}>{softwareDescription}</h2>
         </div>
     );
 }
