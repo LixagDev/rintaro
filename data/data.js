25c7af7acf2fccacc0b8d397d51bde7215628f00
Lixag <valent1.arn@gmail.com>
2024-01-24 15:37:46 +0100

    Add MenuItemsData()

--- data/data.js
+++ data/data.js
@@ -1,10 +1,19 @@
-import {RefreshCw, Youtube} from "lucide-react";
+import {RefreshCw, Youtube, User, Settings} from "lucide-react";
 
 export function SoftwaresData(){
     const softwares = [
         {id: 0, name: "Convertisseur", description: "Convertissez vos images en l'extension que vous souhaitez.", href: "/convert", logo: <RefreshCw size={40}/>, beta: true},
-        {id: 0, name: "Youtube DL", description: "Téléchargez vos videos Youtube au format MP4 ou WEBM.", href: "/youtube", logo: <Youtube size={40}/>, beta: false},
+        {id: 1, name: "Youtube DL", description: "Téléchargez vos videos Youtube au format MP4 ou WEBM.", href: "/youtube-dl", logo: <Youtube size={40}/>, beta: false},
     ];
 
     return softwares;
+}
+
+export function MenuItemsData(){
+    const menuItems = [
+        {name: "Profil", href: "/profil", logo: <User/>, disabled: true},
+        {name: "Paramètres", href: "/settings", logo: <Settings/>, disabled: true}
+    ]
+
+    return menuItems;
 }
