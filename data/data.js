dec3a96adbf711d1b7397bafb8b6721a1865ed68
Lixag <valent1.arn@gmail.com>
2024-01-26 00:25:44 +0100

    Update data.js

--- data/data.js
+++ data/data.js
@@ -2,8 +2,8 @@ import {RefreshCw, Youtube, User, Settings} from "lucide-react";
 
 export function SoftwaresData(){
     const softwares = [
-        {id: 0, name: "Convertisseur", description: "Convertissez vos images en l'extension que vous souhaitez.", href: "/convert", logo: <RefreshCw size={40}/>, beta: true},
-        {id: 1, name: "Youtube DL", description: "Téléchargez vos videos Youtube au format MP4 ou WEBM.", href: "/youtube-dl", logo: <Youtube size={40}/>, beta: false},
+        {id: 0, name: "Convertisseur", description: "Convertissez vos images en l'extension que vous souhaitez.", href: "/convert", logo: <RefreshCw className={"w-4 mr-1"}/>, beta: true},
+        {id: 1, name: "Youtube DL", description: "Téléchargez vos videos Youtube au format MP4 ou WEBM.", href: "/youtube-dl", logo: <Youtube className={"w-4 mr-1"}/>, beta: false},
     ];
 
     return softwares;
@@ -11,7 +11,7 @@ export function SoftwaresData(){
 
 export function MenuItemsData(){
     const menuItems = [
-        {id: 0, name: "Profil", href: "/profil", logo: <User/>, disabled: true},
+        {id: 0, name: "Profil", href: "/profil", logo: <User className={"w-4 mr-1"}/>, disabled: true},
         {id: 1, name: "Paramètres", href: "/settings", logo: <Settings/>, disabled: false}
     ]
 
