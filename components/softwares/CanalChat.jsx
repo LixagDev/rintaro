5c51c5f1ad4554c045e82fbea12d5e19fc59962c
Lixag <valent1.arn@gmail.com>
2024-01-27 22:01:04 +0100

    Test Chat Refresh

--- components/softwares/CanalChat.jsx
+++ components/softwares/CanalChat.jsx
@@ -4,8 +4,10 @@ import {Input} from "@/components/ui/input";
 import {useEffect, useRef, useState} from "react";
 import {sendChat} from "@/functions/DataManager";
 import {io} from "socket.io-client";
+import {useRouter} from "next/navigation";
 
 export default function CanalChat({...props}){
+    const router = useRouter();
     const socket = io("https://api.rintaro.fr:8080")
     const session = props.session;
     const [chats, setChats] = useState(props.chats);
@@ -17,20 +19,7 @@ export default function CanalChat({...props}){
         socket.on("chat",  async (chatSocket) => {
             if (session.user.id !== chatSocket.user.id){
                 console.log(chatSocket)
-                let chatsCopy = await chats.slice();
-                await chatsCopy.push({
-                    id: chatSocket.id,
-                    content: chatSocket.content,
-                    created_at: new Date(chatSocket.created_at).getTime(),
-                    user: {
-                        id: chatSocket.user.id,
-                        name: chatSocket.user.name,
-                        image: chatSocket.user.image,
-                    },
-                });
-                await setChats(chatsCopy);
-                await chat.current.scrollTo({top: chat.current.scrollHeight, behavior: "smooth"});
-                await console.log(chatsCopy)
+                router.refresh();
             }
             else{
                 console.log("non c toi")
