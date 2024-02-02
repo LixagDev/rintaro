c07de35717097de84fea998dd46f928896931e84
Lixag <valent1.arn@gmail.com>
2024-02-02 21:09:09 +0100

    Add gap

--- components/ui/menu-item.jsx
+++ components/ui/menu-item.jsx
@@ -2,7 +2,7 @@
 
 export default function MenuItem({children, ...props}){
     return <button {...props}
-        className={"relative p-1 flex select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors hover:bg-accent hover:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50"}>
+        className={"relative gap-1 p-1 flex select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors hover:bg-accent hover:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50"}>
         {props.icon}{children}
     </button>
 }
