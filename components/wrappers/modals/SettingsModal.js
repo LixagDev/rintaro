8b6a51e12101c67a8ce2a293938e2bafd55b9fbe
Lixag <valent1.arn@gmail.com>
2024-02-03 23:05:50 +0100

    Create SettingsModal.js

---
+++ components/wrappers/modals/SettingsModal.js
@@ -0,0 +1,67 @@
+"use client"
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+    DialogFooter,
+    DialogClose
+} from "@/components/ui/dialog";
+import {Button} from "@/components/ui/button";
+import {Switch} from "@/components/ui/switch";
+import {useRef, useState} from "react";
+import {useRouter} from "next/navigation";
+import {SaveSettings} from "@/functions/DataManager";
+import {toast} from "sonner";
+import {Loader2} from "lucide-react";
+
+export default function SettingsModal({children, ...props}){
+    const router = useRouter();
+    const session = props.session;
+    const close = useRef();
+    const [devMode, setDevMode] = useState(session.user.settings.devMode);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const saveSettings = async () => {
+        setIsLoading(true);
+        await SaveSettings({session, devMode})
+            .then(() => {
+                close.current.click();
+                setIsLoading(false);
+                toast("Paramètres enregistré !", {
+                    action: {
+                        label: "Ok",
+                        onClick: () => null,
+                    },
+                });
+                router.refresh();
+            });
+    }
+
+    return(
+        <Dialog>
+            <DialogTrigger asChild>{children}</DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Paramètres</DialogTitle>
+                    <DialogDescription>
+                        <div className={"flex flex-col"}>
+                            <div className={"flex"}>
+                                <span className={"flex basis-1/2 justify-start"}>Mode développeur</span>
+                                <div className={"flex basis-1/2 justify-end"}><Switch onClick={() => setDevMode(!devMode)} checked={devMode} /></div>
+                            </div>
+                        </div>
+                    </DialogDescription>
+                </DialogHeader>
+                <DialogFooter>
+                    <DialogClose>
+                        <Button ref={close} variant={"secondary"}>Fermer</Button>
+                    </DialogClose>
+                    <Button disabled={isLoading} onClick={saveSettings}>{isLoading ? <Loader2 className={"w-4 mr-2 animate-spin"}/> : null} Enregistrer</Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    );
+}
