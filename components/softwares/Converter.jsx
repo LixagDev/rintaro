a3c1cef1f9a3bf89d095b74ad08fe700b55de3c9
Lixag <valent1.arn@gmail.com>
2024-02-03 23:08:26 +0100

    Update Converter.jsx

--- components/softwares/Converter.jsx
+++ components/softwares/Converter.jsx
@@ -22,9 +22,10 @@ import {useCallback, useState} from "react";
 import axios from "axios";
 import FormData from "form-data";
 import {useRouter} from "next/navigation";
-import {Trash2} from "lucide-react";
+import {Trash2, Loader2} from "lucide-react";
 import {useDropzone} from "react-dropzone";
 import {UpdateImageConvertStat} from "@/functions/DataManager";
+import Debug from "@/components/Debug";
 
 export default function Converter({...props}){
     const session = props.session;
@@ -101,66 +102,76 @@ export default function Converter({...props}){
     const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
 
     return(
-        <div className={"flex flex-col gap-4 items-center"}>
-            {
-                files.length !== 0 ?
-                    <Table className={"backdrop-blur-sm border rounded-xl overflow-hidden border-separate border-spacing-0"}>
-                        <TableHeader className={"sticky top-0"}>
-                            <TableRow>
-                                <TableHead className={"w-1/4"}>Nom</TableHead>
-                                <TableHead className={"w-1/4"}>Taille</TableHead>
-                                <TableHead className={"w-1/4"}>Type</TableHead>
-                                <TableHead className={"w-1/4"}>Status</TableHead>
-                                <TableHead className={"w-1/4"}>Convertir en...</TableHead>
-                                <TableHead className={"w-1/4"}></TableHead>
-                                <TableHead className={"w-1/4"}></TableHead>
-                            </TableRow>
-                        </TableHeader>
-                        <TableBody>
-                            {files.map((file) => (
-                                <TableRow key={file.id}>
-                                    <TableCell className="font-medium">{file.name}</TableCell>
-                                    <TableCell>{convertSize(file.size)}</TableCell>
-                                    <TableCell>{file.type}</TableCell>
-                                    <TableCell>{file.isConvert ? "Terminé" : file.error ? "Erreur" : "Prêt"}</TableCell>
-                                    <TableCell>
-                                        <Select onValueChange={(e) => addExt(e, file.id)}>
-                                            <SelectTrigger className={"w-fit"}>
-                                                <SelectValue placeholder="Choisir une extension"/>
-                                            </SelectTrigger>
-                                            <SelectContent>
-                                                <SelectGroup>
-                                                    <SelectLabel>Extensions</SelectLabel>
-                                                    <SelectItem value="jpg">JPG</SelectItem>
-                                                    <SelectItem value="png">PNG</SelectItem>
-                                                    <SelectItem value="webp">WEBP</SelectItem>
-                                                </SelectGroup>
-                                            </SelectContent>
-                                        </Select>
-                                    </TableCell>
-                                    <TableCell>
-                                        {
-                                            file.convertLink ? <Button onClick={() => router.push(file.convertLink)}>Télécharger</Button>
-                                                : <Button disabled={(file.isLoading || !file.ext)} onClick={() => convertFile(file.id, file)}>Convertir</Button>
-                                        }
-                                    </TableCell>
-                                    <TableCell>
-                                        <Button variant={"destructive"} onClick={() => deleteFile(file.id)}><Trash2/></Button>
-                                    </TableCell>
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                    : null
-            }
-            <div {...getRootProps()} className={"w-80 h-80 border rounded-xl flex justify-center items-center backdrop-blur-sm p-5"}>
-                <input {...getInputProps()} />
+        <>
+            {session.user.settings.devMode ? <Debug>FILES : {JSON.stringify(files)}</Debug> : null}
+            <div className={"flex flex-col gap-4 items-center"}>
                 {
-                    isDragActive ?
-                        <h2>Nickel !</h2> :
-                        <h2 className={"text-center"}>Glissez et déposez vos images ici ou juste cliquez.</h2>
+                    files.length !== 0 ?
+                        <Table
+                            className={"backdrop-blur-sm border rounded-xl overflow-hidden border-separate border-spacing-0"}>
+                            <TableHeader className={"sticky top-0"}>
+                                <TableRow>
+                                    <TableHead className={"w-1/4"}>Nom</TableHead>
+                                    <TableHead className={"w-1/4"}>Taille</TableHead>
+                                    <TableHead className={"w-1/4"}>Type</TableHead>
+                                    <TableHead className={"w-1/4"}>Status</TableHead>
+                                    <TableHead className={"w-1/4"}>Convertir en...</TableHead>
+                                    <TableHead className={"w-1/4"}></TableHead>
+                                    <TableHead className={"w-1/4"}></TableHead>
+                                </TableRow>
+                            </TableHeader>
+                            <TableBody>
+                                {files.map((file) => (
+                                    <TableRow key={file.id}>
+                                        <TableCell className="font-medium">{file.name}</TableCell>
+                                        <TableCell>{convertSize(file.size)}</TableCell>
+                                        <TableCell>{file.type}</TableCell>
+                                        <TableCell>{file.isConvert ? "Terminé" : file.error ? "Erreur" : "Prêt"}</TableCell>
+                                        <TableCell>
+                                            <Select onValueChange={(e) => addExt(e, file.id)}>
+                                                <SelectTrigger className={"w-fit"}>
+                                                    <SelectValue placeholder="Choisir une extension"/>
+                                                </SelectTrigger>
+                                                <SelectContent>
+                                                    <SelectGroup>
+                                                        <SelectLabel>Extensions</SelectLabel>
+                                                        <SelectItem value="jpg">JPG</SelectItem>
+                                                        <SelectItem value="png">PNG</SelectItem>
+                                                        <SelectItem value="webp">WEBP</SelectItem>
+                                                    </SelectGroup>
+                                                </SelectContent>
+                                            </Select>
+                                        </TableCell>
+                                        <TableCell>
+                                            {
+                                                file.convertLink ? <Button
+                                                        onClick={() => router.push(file.convertLink)}>Télécharger</Button>
+                                                    : <Button disabled={(file.isLoading || !file.ext)}
+                                                              onClick={() => convertFile(file.id, file)}>{file.isLoading ? <>
+                                                        <Loader2
+                                                            className={"w-4 mr-2"}/> Conversion</> : "Convertir"}</Button>
+                                            }
+                                        </TableCell>
+                                        <TableCell>
+                                            <Button variant={"destructive"}
+                                                    onClick={() => deleteFile(file.id)}><Trash2/></Button>
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                        : null
                 }
+                <div {...getRootProps()}
+                     className={"w-80 h-80 border rounded-xl flex justify-center items-center backdrop-blur-sm p-5"}>
+                    <input {...getInputProps()} />
+                    {
+                        isDragActive ?
+                            <h2>Nickel !</h2> :
+                            <h2 className={"text-center"}>Glissez et déposez vos images ici ou juste cliquez.</h2>
+                    }
+                </div>
             </div>
-        </div>
+        </>
     );
 }
