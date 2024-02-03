e3dffae1b24cc04213957612b51fe0e5b1986273
Lixag <valent1.arn@gmail.com>
2024-02-03 23:06:30 +0100

    Delete callback

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,10 +2,7 @@ import NextAuth from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 import {PrismaAdapter} from "@next-auth/prisma-adapter";
 import {PrismaClient} from "@prisma/client";
-
 const prisma = new PrismaClient();
-let userId;
-let userStats;
 
 export const authOptions = {
     adapter: PrismaAdapter(prisma),
@@ -16,48 +13,6 @@ export const authOptions = {
         })
     ],
     secret: process.env.NEXTAUTH_SECRET,
-    callback:{
-        async signIn({ user, account, profile, email, credentials }) {
-            const isAllowedToSignIn = true;
-            if (isAllowedToSignIn) {
-                userId = user.id;
-                userStats = await prisma.stat.findUnique({
-                    where:{
-                        userId: userId
-                    },
-                    select:{
-                        imageConvert: true,
-                        youtubeDl: true,
-                    }
-                });
-                if (!userStats){
-                    const createStatTable = await prisma.stat.create({
-                        data:{
-                            userId: userId
-                        }
-                    }).then(async () => {
-                        userStats = await prisma.stat.findUnique({
-                            where:{
-                                userId: sessionFinal.user.id
-                            },
-                            select:{
-                                imageConvert: true,
-                                youtubeDl: true,
-                            }
-                        });
-                    });
-                }
-                await prisma.$disconnect();
-                return true;
-            } else {
-                return false;
-            }
-        },
-        async session({ session, user, token }) {
-            session.user.id = userId;
-            return session;
-        },
-    }
 }
 
 export default NextAuth(authOptions);
