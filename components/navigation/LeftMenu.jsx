7c9e44ae68fc99633c3ddd92b80b78b10e0b9119
Lixag <valent1.arn@gmail.com>
2024-02-03 23:06:58 +0100

    Update LeftMenu.jsx

--- components/navigation/LeftMenu.jsx
+++ components/navigation/LeftMenu.jsx
@@ -7,6 +7,7 @@ import {signOut} from "next-auth/react";
 import {SoftwaresData} from "@/data/data";
 import {Separator} from "@/components/ui/separator";
 import {Badge} from "@/components/ui/badge";
+import SettingsModal from "@/components/wrappers/modals/SettingsModal";
 
 export default function LeftMenu({...props}){
     const router = useRouter();
@@ -14,36 +15,43 @@ export default function LeftMenu({...props}){
     const session = props.session;
 
     return(
-        <div className={"flex flex-col w-full border-r-2 h-full p-4 bg-background"}>
-            <div className={"flex flex-col basis-1/3"}>
-                <div className={"flex gap-2 items-center"}>
-                    <Avatar className={"w-12 h-12"}>
-                        <AvatarImage draggable={false} src={session.user.image}></AvatarImage>
-                        <AvatarFallback>{session.user.name[0].toUpperCase()}</AvatarFallback>
-                    </Avatar>
-                    <div className={"flex flex-col"}>
-                        <span className={"font-bold"}>{session.user.name}</span>
-                        <span className={"text-zinc-500 text-sm"}>{session.user.email}</span>
+        <>
+            <div className={"flex flex-col w-full border-r-2 h-full p-4 bg-background"}>
+                <div className={"flex flex-col basis-1/3"}>
+                    <div className={"flex gap-2 items-center"}>
+                        <Avatar className={"w-12 h-12"}>
+                            <AvatarImage draggable={false} src={session.user.image}></AvatarImage>
+                            <AvatarFallback>{session.user.name[0].toUpperCase()}</AvatarFallback>
+                        </Avatar>
+                        <div className={"flex flex-col"}>
+                            <span className={"font-bold"}>{session.user.name}</span>
+                            <span className={"text-zinc-500 text-sm"}>{session.user.email}</span>
+                        </div>
                     </div>
                 </div>
+                <div className={"flex flex-col justify-center basis-1/3"}>
+                    <MenuItem onClick={() => router.push("/")} icon={<Home className={"w-4 mr-1"}/>}>Accueil</MenuItem>
+                    <Separator className={"mt-1 mb-1"}/>
+                    {
+                        softwares.map((software) => {
+                            return (
+                                <MenuItem onClick={() => router.push(software.href)}
+                                          icon={software.logo}>{software.name} {software.beta ?
+                                    <Badge>Beta</Badge> : null}</MenuItem>
+                            );
+                        })
+                    }
+                </div>
+                <div className={"flex flex-col flex-col-reverse basis-1/3"}>
+                    <MenuItem onClick={() => signOut()} icon={<LogOut className={"w-4 mr-1"}/>}>Se
+                        déconnecter</MenuItem>
+                    <SettingsModal session={session}><MenuItem icon={<Settings className={"w-4 mr-1"}/>}>Paramètres</MenuItem></SettingsModal>
+                    <MenuItem onClick={() => router.push("https://github.com/LixagDev/rintaro")}
+                              icon={<Github className={"w-4 mr-1"}/>}>GitHub</MenuItem>
+                    <MenuItem onClick={() => router.push(`/profile/${session.user.name}`)}
+                              icon={<User className={"w-4 mr-1"}/>}>Profil</MenuItem>
+                </div>
             </div>
-            <div className={"flex flex-col justify-center basis-1/3"}>
-                <MenuItem onClick={() => router.push("/")} icon={<Home className={"w-4 mr-1"}/>}>Accueil</MenuItem>
-                <Separator/>
-                {
-                    softwares.map((software) => {
-                        return(
-                            <MenuItem onClick={() => router.push(software.href)} icon={software.logo}>{software.name} {software.beta ? <Badge>Beta</Badge> : null}</MenuItem>
-                        );
-                    })
-                }
-            </div>
-            <div className={"flex flex-col flex-col-reverse basis-1/3"}>
-                <MenuItem onClick={() => signOut()} icon={<LogOut className={"w-4 mr-1"}/>}>Se déconnecter</MenuItem>
-                <MenuItem icon={<Settings className={"w-4 mr-1"}/>}>Paramètres</MenuItem>
-                <MenuItem onClick={() => router.push("https://github.com/LixagDev/rintaro")} icon={<Github className={"w-4 mr-1"}/>}>GitHub</MenuItem>
-                <MenuItem onClick={() => router.push(`/profile/${session.user.name}`)} icon={<User className={"w-4 mr-1"}/>}>Profil</MenuItem>
-            </div>
-        </div>
-    )
+        </>
+    );
 }
