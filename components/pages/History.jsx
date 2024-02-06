a2dfcd2b2518c403695cd481b929f3ffa19c6c8e
Lixag <valent1.arn@gmail.com>
2024-02-06 15:58:59 +0100

    Create History.jsx

---
+++ components/pages/History.jsx
@@ -0,0 +1,23 @@
+"use client"
+import LeftMenu from "@/components/navigation/LeftMenu";
+import Title from "@/components/navigation/Title";
+import HistoryTable from "@/components/HistoryTable";
+
+export default function History({...props}){
+    const session = props.session;
+    const userHistory = props.userHistory;
+
+    return(
+        <div className={"lg:flex h-full hidden"}>
+            <div className={"basis-1/6 h-full"}>
+                <LeftMenu session={session}/>
+            </div>
+            <div className={"basis-5/6 h-full flex flex-col overflow-y-auto overflow-x-hidden"}>
+                <Title>Historique</Title>
+                <div className={"h-full w-full p-4 flex flex-col gap-5"}>
+                    <HistoryTable session={session} userHistory={userHistory} />
+                </div>
+            </div>
+        </div>
+    );
+}
