3152df4b8bdda6cf2155357cf97d2e361ae3a668
Lixag <valent1.arn@gmail.com>
2024-02-06 15:58:47 +0100

    Create page.jsx

---
+++ app/history/page.jsx
@@ -0,0 +1,16 @@
+import {getServerSession} from "next-auth";
+import {authOptions} from "@/pages/api/auth/[...nextauth]";
+import {init, GetUserHistory} from "@/functions/DataManager";
+import {redirect} from "next/navigation";
+import History from "@/components/pages/History";
+
+export default async function index({params}){
+    let session = await getServerSession(authOptions);
+
+    if (session){
+        session = await init(session);
+        const userHistory = await GetUserHistory(session);
+        return <History session={session} userHistory={userHistory}/>
+    }
+    else redirect("/");
+}
